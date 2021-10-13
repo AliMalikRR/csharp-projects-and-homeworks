e0cc9ba9283c5082c2622f794258b8448c793d13
AliMalikRR <78763016+AliMalikRR@users.noreply.github.com>
2021-10-13 22:10:32 +0100

    a start on the homework

--- SP week 3 homework/SP week 3 homework/Program.cs
+++ SP week 3 homework/SP week 3 homework/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SP_week_3_homework
 {
@@ -6,7 +8,26 @@ namespace SP_week_3_homework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "W3 Homework";
+
+            // The question is : given a list of usernames use the where functionto filter it gto create a collection containing only usernames that contain_an_underscore.
+
+            List<string> userNames = new List<string>(); // create a list to store the usernames 
+
+             // hardcode some random usernames into the list some of which have under_scores -  this can be improved later on 
+
+            userNames.Add("Ali_Malik");
+            userNames.Add("BigMan");
+            userNames.Add("fezpunch");
+            userNames.Add("whackmuumuu");
+            userNames.Add("reevesalesman");
+            userNames.Add("lecturersnappy");
+            userNames.Add("food_eat");
+            userNames.Add("tractor_trac");
+            userNames.Add("playerkikllal");
+            userNames.Add("ManGracie");
+
+            // now we need to filter these usernames using the where function 
         }
     }
 }
