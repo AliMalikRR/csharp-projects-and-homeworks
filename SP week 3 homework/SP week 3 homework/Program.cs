5d02e2721105ca80449dd0cd0a7e9e0a99bf3cf3
AliMalikRR <78763016+AliMalikRR@users.noreply.github.com>
2021-10-13 21:57:48 +0100

    week 3 homework file

---
+++ SP week 3 homework/SP week 3 homework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SP_week_3_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
