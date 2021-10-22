99644123b23238993aec106f2805e0430b643bc4
Big A <78763016+AliMalikRR@users.noreply.github.com>
2021-10-22 17:26:30 +0100

    as

--- Weel 3 worksheet questions/Weel 3 worksheet questions/Program.cs
+++ Weel 3 worksheet questions/Weel 3 worksheet questions/Program.cs
@@ -9,8 +9,9 @@ namespace Weel_3_worksheet_questions
     {
         static void Main(string[] args)
         {
-            //essential2();
-            //essential3();
+            // essential2();
+            // essential3();
+            essential4();
         }
         public static void essential2()
         {
@@ -40,7 +41,7 @@ namespace Weel_3_worksheet_questions
             cars.Add(new car("merc", "g wagon", "abc1234567", 1994));
             cars.Add(new car("toyota", "yaris", "abc12345678", 1957));
 
-            foreach (car cr in cars.OrderBy(element => element.yearofman)) // order by sorts in asscending order
+            foreach (car cr in cars.OrderBy(element => element.yearofman)) // OrderBy sorts in asscending order
             {
                 Console.WriteLine(cr.yearofman);
             }
@@ -51,7 +52,7 @@ namespace Weel_3_worksheet_questions
             public string model;
             public string reg;
             public int yearofman;
-            public car(string make, string model, string reg, int yearofman)
+            public car(string make, string model, string reg, int yearofman) // constructior 
             {
                 this.make = make;
                 this.model = model;
@@ -61,7 +62,34 @@ namespace Weel_3_worksheet_questions
         }
         public static void essential4()
         {
+            List<react> reacts = new List<react>();
+            // use the max function to find out the max width, then use the max function to find the max height then use a variant of the sum function to find the area.
+            reacts.Add(new react(5, 10));
+            reacts.Add(new react(6, 11));
+            reacts.Add(new react(5.8, 15.6));
+            reacts.Add(new react(6.7, 18.5));
+            reacts.Add(new react(124.5, 12.4));
+            reacts.Add(new react(325.3, 33));
+            reacts.Add(new react(95, 45.2));
+            reacts.Add(new react(23.6, 1224.66));
+            reacts.Add(new react(4633.3, 12.4));
 
+            double biggestheight = reacts.Max(element => element.height);
+            Console.WriteLine("The biggest height is " + biggestheight);
+
+            double biggestwidth = reacts.Max(element => element.width);
+            Console.WriteLine("The biggest width is " + biggestwidth);
+        }
+        public class react
+        {
+            public double width;
+            public double height;
+
+            public react(double width, double height)
+            {
+                this.width = width;
+                this.height = height;
+            }
         }
     }
 }
