c9ad38174fa0c848e8ad83e0efeaba96a97f2460
Big A <78763016+AliMalikRR@users.noreply.github.com>
2021-11-04 15:24:23 +0000

    aaa

---
+++ homework2/homework2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace homework2
+{
+    class Program
+    {
+        static void consoleColour(string message, ConsoleColor colour) // use for error messages only (Red)
+        {
+            Console.ForegroundColor = colour;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+        static void Main(string[] args)
+        {
+            // Homework 2 
+            if (args.Length == 0)
+            {
+                consoleColour("You did not enter anything", ConsoleColor.Cyan);
+            }
+            else if (args.Length == 3)
+            {
+                float n1 = 0; // we set our numbers to 0 so there a value for them even if our try block fails
+                float n2 = 0;
+                string op = args[1];
+                try
+                {
+                    n1 = Convert.ToSingle(args[0]);
+                    n2 = Convert.ToSingle(args[2]);
+                }
+                catch
+                {
+                    consoleColour("Error you can only enter numbers", ConsoleColor.Cyan);
+                    Environment.Exit(0); // close the program if we hit this catch cause there no point of carrying on
+                }
+                if (args[1] == "+")
+                {
+                    Console.WriteLine(n1 + n2);
+                }
+                else if (args[1] == "-")
+                {
+                    Console.WriteLine(n1 - n2);
+                }
+                else if (args[1] == "x")
+                {
+                    Console.WriteLine(n1 * n2);
+                }
+                else if (args[1] == "/")
+                {
+                    Console.WriteLine(n1 / n2);
+                }
+                else if (args[1] != "+" || args[1] != "-" || args[1] != "x" || args[1] != "/" || args[1] == "")
+                {
+                    consoleColour("Unknow operator try use '+', '-', 'x' or '/'", ConsoleColor.Cyan);
+                }
+            }
+            else if(args.Length != 3)
+            {
+                consoleColour("oops, you did not format your question correctly", ConsoleColor.Cyan);
+            }
+
+        }
+
+    }
+
+ }
