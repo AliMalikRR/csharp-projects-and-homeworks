5c848ca65506733d3e6b5d8eddadddb23b49be4f
AliMalikRR <78763016+AliMalikRR@users.noreply.github.com>
2021-11-05 00:37:44 +0000

    some console colour updates

--- homework2/homework2/Program.cs
+++ homework2/homework2/Program.cs
@@ -18,22 +18,21 @@ namespace homework2
             // Homework 2 
             if (args.Length == 0)
             {
-                consoleColour("You did not enter anything", ConsoleColor.Cyan);
+                consoleColour("ERROR, you did not enter anything", ConsoleColor.Red);
             }
             else if (args.Length == 3)
             {
-                float n1 = 0; // we set our numbers to 0 so there a value for them even if our try block fails
+                float n1 = 0; // we set our numbers to 0 so there a value for them even if our try catch block fails
                 float n2 = 0;
                 string op = args[1];
                 try
                 {
-                    // if they enter a string in postiion 0 or 2 there will be an error as you cant convert a string to a float so we move into the catch block.
                     n1 = Convert.ToSingle(args[0]);
                     n2 = Convert.ToSingle(args[2]);
                 }
                 catch
                 {
-                    consoleColour("Error you can only enter numbers", ConsoleColor.Cyan);
+                    consoleColour("ERROR, you can only enter numbers", ConsoleColor.Red);
                     Environment.Exit(0); // close the program if we hit this catch cause there no point of carrying on
                 }
                 if (args[1] == "+")
@@ -54,12 +53,14 @@ namespace homework2
                 }
                 else if (args[1] != "+" || args[1] != "-" || args[1] != "x" || args[1] != "/" || args[1] == "")
                 {
-                    consoleColour("Unknow operator try use '+', '-', 'x' or '/'", ConsoleColor.Cyan);
+                    consoleColour("ERROR, unknow operator: ", ConsoleColor.Red);
+                    consoleColour("try use: '+', '-', 'x', '/'", ConsoleColor.Green);
                 }
             }
             else if(args.Length != 3)
             {
-                consoleColour("oops, you did not format your question correctly", ConsoleColor.Cyan);
+                consoleColour("oops, you did not format your question correctly", ConsoleColor.Red);
+                consoleColour("try enter something like: 'x + x' or 'x / x' ", ConsoleColor.Green);
             }
 
         }
